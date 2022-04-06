bb327114982a54deb73b7c035258b6131119a75e
rluks <rluks+x@pm.me>
2022-04-06 18:32:10 +0200

    UI redesign

--- include/embed-twine-upload-file.php
+++ include/embed-twine-upload-file.php
@@ -29,23 +29,36 @@ function embed_twine_your_custom_callback($dir, $name, $ext){
 
 ?>
 <div class="wrap">
-<h1>Upload Twine story</h1>
+<h1>Embed your story in 4 steps</h1>
 
-<!-- Form -->
+<ol>
+  <li>Export story from Twine</li>
+  <li>Upload it</li>
+  <li>Copy shortcode</li>
+  <li>Paste shortcode on a page</li>
+</ol> 
+
+<h2>1. Export story from Twine</h2>
+<p>Open your story in Twine and select "Publish to File".</p>
+
+<h2>2. Upload it</h2>
+<p>Upload the exported .html file here: </p>
+
 <form method='post' action='' name='myform' enctype='multipart/form-data'>
   <ul>
-      <li><label for="embed_twine_file_select">Select file</label>
-      <input class='button-secondary' id="embed_twine_file_select" type='file' name='file' accept="text/html"></li>
-      <li><label for="embed_twine_file_submit">Upload it</label>
-      <input class='button-primary' id="embed_twine_file_submit" type='submit' name='but_submit' value='Submit'></li>
+      <li><input class='button-secondary' id="embed_twine_file_select" type='file' name='file' accept="text/html"></li>
+      <li><input class='button-primary' id="embed_twine_file_submit" type='submit' name='but_submit' value='Upload'></li>
   </ul>
 </form>
 
-<p>Original Twine story file is uploaded and stored in the uploads directory. <br>
-  Processed and modified version for embedding is stored in plugin subfolder in the uploads directory.<br>
-  Upon completion, you'll be provided with a shortcode. Insert this shortcode into post or page to embed your story.<br>
-</p>
-<br>
-<br>
+<h2>3. Copy shortcode</h2>
+<p>Copy this shortcode:</p>
+
+<input type="text" value="[embed_twine story=&quot;Story&quot;]" readonly="" id="embed_twine_shortcode" size="30">
+<button class='button-primary' id="embed_twine_copy_shortcode">Copy</button>
+
+<h2>4. Paste the shortcode on a page</h2>
+<p>Insert the shortcode to any page or post, save it and enjoy your embedded story.</p>
+
 <p><i>Hope this plugin serves you well!<br> You can appreciate the effort put into it by <a target="_blank" href="https://wordpress.org/support/plugin/embed-twine/reviews/">rating it</a> and providing feedback, thank you.</i></p>
 </div>
